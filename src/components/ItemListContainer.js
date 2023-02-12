3b84f35f46912ae9f3ace2d2d7caa1c742c4c2c1
Marcelo Gómez <marcelo@pop-os.localdomain>
2023-02-12 15:52:18 -0300

    agregado filtrado de categorías

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import GridProductos from './GridProductos';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ListaCategorias from './ListaCategorias.js';
 import Loading from './Loading';
+import { collection, getDocs, getFirestore, limit, query, where } from 'firebase/firestore';
 
 
 export default function ItemListContainer() {
 
-  const [listaProductos, setListaProductos] = useState();
-  const catId = useParams();
+    const [listaProductos, setListaProductos] = useState();
+    const catId = useParams();
 
-  useEffect( () => {
-    if(catId.id){
-      fetch(`https://fakestoreapi.com/products/category/${catId.id}`)
-      .then(resp => resp.json())
-      .then(respJson => setListaProductos(respJson));
-      
-    } else {
-      fetch('https://fakestoreapi.com/products')
-      .then(resp => resp.json())
-      .then(respJson => setListaProductos(respJson));
+    function construirQuery(db) {
+        const queryArgs = [collection(db, "items")];
+        if (catId.id) queryArgs.push(where("category", "==", catId.id));
+
+        return query(...queryArgs);
     }
-    
-  }, [catId] );
 
-  return (
-    <>
-    <ListaCategorias />
-      {listaProductos ? <GridProductos productos={listaProductos} /> : <Loading/>}
-    </>
-  );
+    useEffect(() => {
+        const q = construirQuery(getFirestore());
+        getDocs(q).then((snapshot) => {
+            if (snapshot.size === 0) {
+                console.log("Sin resultados");
+            } else {
+                setListaProductos(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+            }
+        });
+    }, [catId]);
+
+    return (
+        <>
+            <ListaCategorias />
+            {listaProductos ? <GridProductos productos={listaProductos} /> : <Loading />}
+        </>
+    );
 }
